1a940705743767553f4b5506bc7c6fcab86528a0
EC2 Default User <ec2-user@ip-172-31-34-246.ec2.internal>
2018-11-02 00:32:16 +0000

    add messages file

---
+++ messages.php
@@ -0,0 +1,25 @@
+<?php include('includes/connection.php');?>
+
+<?php include('includes/header2.php');?>
+
+<?php
+	if ((!isset($_SESSION['user_id']) && $_SESSION['user_id']=='')) {
+		header('Location:'.$base_path);
+	        exit;		
+	}
+?>
+<section class="about_outer">
+    <div class="container">
+        <div class="about_cont clearfix">
+		<div class="about_left col-lg-4 col-md-4 col-sm-4 col-xs-12">
+    			<h3>Messages</div>
+	    	</div>
+	</div>
+    <div>
+</section>
+
+<?php include('includes/footer.php');?>
+
+</body>
+
+</html>
