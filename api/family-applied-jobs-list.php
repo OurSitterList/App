ce242cdf6dd336c8d0d8c140ff3fc0d47352e80b
Eric Kambestad <eric@elmingtontech.com>
2018-11-12 21:15:30 -0600

    try thius

--- api/family-applied-jobs-list.php
+++ api/family-applied-jobs-list.php
@@ -1,12 +1,9 @@
-<?php include('../includes/connection.php');
+<?php include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 
   error_reporting(E_ALL);
   ini_set("display_errors", 1);
 
-  include('includes/connection.php');
-  //var_dump($_SESSION);
-  // var_dump('test');
-  // exit;
+  include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
   $ser_id = (isset($_GET['user_id'])) ? trim($_GET['user_id']) : NULL;
 
   if (!$user_id) {
@@ -15,14 +12,10 @@
   }
 
   $search_query_sql = "select * from jobapply_management WHERE sitter_user_id='".$user_id."' order by applytime;";
-  var_dump($search_query_sql);
   $search_query = mysql_query($search_query_sql);
-  var_dump(mysql_num_rows($search_query));
-  var_dump($search_query); exit;
   $results = array();
   if (mysql_num_rows($search_query) > 0) {
     $available = 1;
-    var_dump('derp'); exit;
     while($R = mysql_fetch_object($search_query)) {
       $job_query = mysql_query("select * from `job_management` where set_code='".$R->job_id."'");
       $datearr =array();
@@ -37,13 +30,18 @@
       $totaldate1 = implode(', ',$datearr1);
       $job_history =  mysql_fetch_object(mysql_query("select * from `job_management` where set_code='".$R->job_id."'"));
 
+      while ($job = mysql_fetch_object($job_history)) {
+        $data[] = $job;
+      }
+
       $results[] = array(
         'totalDate' => $totaldate,
         'totalDate1' =>  $totaldate1,
-        'results' => $job_history,
+        'results' => $data,
         'job' => $R,
       );
     }
   }
 
-  return json_encode(array('code' => 200, 'message' => $results));
+  $response = array('code' => 200, 'message' => $results);
+  echo json_encode($response); exit;
