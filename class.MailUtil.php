00d11ded0dc6b0c955d6c0af4a552c01d401c30c
Eric Kambestad <eric@elmingtontech.com>
2018-11-13 17:07:48 -0600

    debug

--- class.MailUtil.php
+++ class.MailUtil.php
@@ -19,7 +19,6 @@ class MailUtil
     {
         $mail = new PHPMailer;
         $mail->IsSMTP();
-        $mail->SMTPDebug  = 1;
 
 //        $mail->SMTPDebug  = 1;
         $mail->Host = "smtp.gmail.com";
