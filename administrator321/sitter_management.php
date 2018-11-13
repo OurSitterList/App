7e105b4cb9b873d385787158babd86842a29af42
Eric Kambestad <eric@elmingtontech.com>
2018-11-13 14:42:28 -0600

    remove debugging

--- administrator321/sitter_management.php
+++ administrator321/sitter_management.php
@@ -1,8 +1,4 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 	include("config/admin-includes.php");
 	include("templates/sitter_management-tpl.php");	
 	$page=new Settings("Sitter");
