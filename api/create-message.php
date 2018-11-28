1dee0fd7646c6fdda33563902a8233f114516e4d
Eric Kambestad <eric@elmingtontech.com>
2018-11-27 22:35:35 -0600

    one more format

--- api/create-message.php
+++ api/create-message.php
@@ -8,7 +8,7 @@ include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 extract($_POST);
 
 $sql = "INSERT INTO messages (user_id, recipient_id, thread_id, message, created_at)
-  VALUES ('" . $user_id . "', '" . $recipient_id . "', '" . $thread_id . "', '" . $message . "', '" . time() . "')";
+  VALUES ('" . $user_id . "', '" . $recipient_id . "', '" . $thread_id . "', '" . $message . "', '" . date('Y-m-d H:i:s',time()) . "')";
 
 mysql_query($sql);
 
