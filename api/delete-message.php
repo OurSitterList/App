8c9eb459babd0fe14bfc581109a6655bbbeab079
Eric Kambestad <eric@elmingtontech.com>
2018-11-27 22:45:59 -0600

    adds message delete

---
+++ api/delete-message.php
@@ -0,0 +1,15 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
+
+extract($_POST);
+
+$sql = "UPDATE messages SET deleted_at='" . date('Y-m-d H:i:s',time()) . "' WHERE id='" . $message_id . "';";
+
+mysql_query($sql);
+
+$response	= array('code' => 200, 'message' => 'Success.');
+echo json_encode($response); exit;
