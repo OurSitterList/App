5eb01a3a2a9666555fec692dae3e15d7b20a40fe
Eric Kambestad <eric@elmingtontech.com>
2018-11-27 22:21:13 -0600

    add create message

---
+++ api/create-message.php
@@ -0,0 +1,17 @@
+<?php
+
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
+
+include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
+
+// var_dump($_POST);
+// exit;
+
+extract($_POST);
+
+$sql = "INSERT INTO messages (user_id, recipient_id, thread_id, message, created_at)
+  VALUES ('" . $user_id . "', '" . $recipient_id . "', '" . $thread_id . "', '" . $message . "', '" . date() . "');
+
+print_r($sql);
+exit;
