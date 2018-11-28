826f82bdb76ef3cd800c54f86c17610a096fbe9a
Eric Kambestad <eric@elmingtontech.com>
2018-11-27 22:31:59 -0600

    finish create message

--- api/create-message.php
+++ api/create-message.php
@@ -5,13 +5,12 @@ ini_set("display_errors", 1);
 
 include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 
-var_dump($_POST);
-// exit;
-
 extract($_POST);
 
 $sql = "INSERT INTO messages (user_id, recipient_id, thread_id, message, created_at)
   VALUES ('" . $user_id . "', '" . $recipient_id . "', '" . $thread_id . "', '" . $message . "', '" . date('m/d/Y h:i:s a', time()) . "')";
 
-var_dump($sql);
-exit;
+mysql_query($sql);
+
+$response	= array('code' => 200, 'message' => 'Success.');
+echo json_encode($response); exit;
