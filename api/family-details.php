68c3abe016cc96cac2cd2428e3ea984bce1591b7
Eric Kambestad <eric@elmingtontech.com>
2018-11-27 21:59:37 -0600

    fix family deets

--- api/family-details.php
+++ api/family-details.php
@@ -5,16 +5,20 @@
 
   include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 
+  if (!$_REQUEST['user_id']) {
+    return json_encode(array('code' => 401, 'message' => 'User ID is required.'));
+  }
+
   $sql = mysql_query("select * from user_information ui 
     left join user_management um ON ui.user_id = um.user_id
-    where ui.user_id='".base64_decode($_REQUEST['user_id'])."' AND um.user_type='family' AND user_status=1");
+    where ui.user_id='".$_REQUEST['user_id']."' AND um.user_type='family' AND user_status=1");
 														
   $query = mysql_query($sql);
   $num = mysql_num_rows($query);
   $data = array();
   
-  while ($job = mysql_fetch_object($query)) {
-      $data[] = $job;
+  while ($R = mysql_fetch_object($query)) {
+      $data[] = $R;
   }
   
   $response = array('code' => 200, 'message' => array('results' => $data, 'total' => $num));
