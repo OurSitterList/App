104aefcd1241ad60b110c60fa274e7add9196d77
Eric Kambestad <eric@elmingtontech.com>
2018-12-04 20:28:17 -0600

    make sure isset

--- api/messages.php
+++ api/messages.php
@@ -5,7 +5,7 @@
 
   include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 
-  if (!isset($_REQUEST) || !$_REQUEST['user_id']) {
+  if (!isset($_REQUEST) || !array_key_exists('user_id', $_REQUEST)) {
     return json_encode(array('code' => 401, 'message' => 'User ID is required.'));
   }
 
