1380e95b26224957f5ec4505e3239035da4cc50e
Eric Kambestad <eric@elmingtontech.com>
2018-12-04 21:31:04 -0600

    reset passwor dendpoint

---
+++ api/reset-password.php
@@ -0,0 +1,22 @@
+<?php
+
+  // error_reporting(E_ALL);
+  // ini_set("display_errors", 1);
+
+  include('includes/connection.php');
+
+  /**
+   * POST
+   * 
+   * hidden_code
+   * sitter_new_password
+   */
+
+  extract($_POST);
+
+  mysql_query("update user_management set
+    user_password  = '".mysql_real_escape_string(md5($sitter_new_password))."'
+    where user_code='".$hidden_code."'");
+
+  $response	= array('code' => 200, 'message' => 'Success.');
+  echo json_encode($response); exit;
