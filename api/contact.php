8718b73c0fe949409ad63b9389aeb3328b62905a
Eric Kambestad <eric@elmingtontech.com>
2018-12-04 20:46:49 -0600

    contact endpoint

---
+++ api/contact.php
@@ -0,0 +1,26 @@
+<?php
+
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
+
+include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
+
+extract($_POST);
+
+$notification 		= new Notification();
+
+$name	= trim($contact_name);
+$email	= trim($contact_email);
+$as		= trim($contact_as);
+$msg	= trim($contact_comment);
+
+if(!empty($name) || !empty($email) || !empty($as) || !empty($msg))
+{
+	$details	= array	('name'	=>$name , 'email'	=>$email , 'as'	=>$as , 'msg'	=>$msg );
+	$response 	= $notification->send_contact_form_email($details);
+	$response	= array('code' => 200, 'message' => $response);
+  echo json_encode($response); exit;
+}
+
+$response	= array('code' => 400, 'message' => 'Contact form failed to send.');
+echo json_encode($response); exit;
