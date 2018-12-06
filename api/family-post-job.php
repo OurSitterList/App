12ba93e3701e3e326cd548e02a1f5431bad5b200
Eric Kambestad <eric@elmingtontech.com>
2018-12-05 22:13:49 -0600

    add family job post

---
+++ api/family-post-job.php
@@ -0,0 +1,24 @@
+<?php
+
+  include('includes/connection.php');
+
+	$code =  time().rand(0,100);
+	$calender_val_arr = explode(',',$job_calender_val);
+	$insertedId = null;
+	foreach($calender_val_arr as $CV) {
+		$make_id = str_replace('/','',trim($CV));
+		mysql_query("insert into job_management set set_code='".$code."',family_user_id='".$user_id."',booking_date='".trim($CV)."',booking_placed_date='".time()."',booking_status='1',start_time='".$_REQUEST['job_start_time'.$make_id]."',end_time='".$_REQUEST['job_end_time'.$make_id]."',no_of_kids='" . mysql_real_escape_string($_REQUEST['job_no_of_kids']) . "',location_code='".$_REQUEST['job_location_code']."',remarks='" . mysql_real_escape_string($_REQUEST['job_remarks']) . "', location_id = '" . $location_id . "'");
+
+		if (!$insertedId)
+		{
+			$insertedId = mysql_insert_id();
+		}
+
+	}
+
+	// send notification
+	$notification 		= new Notification();
+	$notification->send_family_notification($_REQUEST, $insertedId);
+	$response = array('code' => 200, 'message' => 'Your job posting has been created.');
+  echo json_encode($response); exit;
+  
