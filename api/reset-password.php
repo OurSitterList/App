f3e6af6701f34722ced7fcff210c84750fd87948
Eric Kambestad <eric@elmingtontech.com>
2018-12-27 09:24:24 -0600

    better connection

--- api/reset-password.php
+++ api/reset-password.php
@@ -1,9 +1,10 @@
 <?php
 
+
   // error_reporting(E_ALL);
   // ini_set("display_errors", 1);
 
-  include('includes/connection.php');
+  include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 
   /**
    * POST
