f5b94d795a2350b6307c6e65af3cb7297fc9b4ed
Adam Horky <adam.horky06@gmail.com>
2018-12-28 14:51:05 -0600

    new API for saving a push notification for a User

---
+++ api/update-push-token.php
@@ -0,0 +1,27 @@
+<?php
+
+  include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
+
+  /**
+   * POST
+   *
+   * user_id
+   * push_token
+   */
+
+  extract($_POST);
+
+  if (!$user_id) {
+    $response = array('code' => 500, 'message' => 'User ID is required.');
+    echo json_encode(array('code' => 500, 'message' => 'User ID is required.')); exit;
+  }
+
+  $search_query = mysql_query("select * from  user_information where user_id='".$user_id."'");
+  if (mysql_num_rows($search_query) > 0) {
+    mysql_query("insert into push_tokens set user_id='".$user_id."', push_token='".$push_token."'");
+    $response = array('code' => 200, 'message' => 'Success.');
+    echo json_encode($response); exit;
+  } else {
+    $response = array('code' => 500, 'message' => 'Valid User Id is required.');
+    echo json_encode(array('code' => 500, 'message' => 'Valid User Id is required.')); exit;
+  }
