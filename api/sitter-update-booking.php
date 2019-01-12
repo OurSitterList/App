96adf2b960f899ef9f7051a2513f8420995a7161
Adam Horky <adam.horky06@gmail.com>
2019-01-12 16:42:12 -0600

    new API to approve/decline booking by sitter

---
+++ api/sitter-update-booking.php
@@ -0,0 +1,34 @@
+<?php
+
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
+
+include $_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php';
+
+if (!isset($_REQUEST) || !array_key_exists('user_id', $_REQUEST)) {
+    echo json_encode(array('code' => 401, 'message' => 'User ID is required.'));
+    exit;
+}
+
+/**
+ * POST
+ *
+ * $user_id
+ * $book_id
+ * $sitter_approval
+ */
+
+try {
+    $search_query = mysql_query("select * from book_management where book_id='" . $book_id . "'");
+    if (mysql_num_rows($search_query) > 0) {
+        $response = array('code' => 500, 'message' => 'success');
+        echo json_encode($response);exit;
+    } else {
+        $response = array('code' => 500, 'message' => 'No booking found that matches the id.');
+        echo json_encode($response);exit;
+    }
+} catch (Exception $e) {
+    $response = array('code' => 400, 'message' => 'Error.');
+    echo json_encode($response);
+    exit;
+}
