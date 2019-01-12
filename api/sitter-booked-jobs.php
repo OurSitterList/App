1bc362c55545805b574f0a0eaa7cb6c1e3334b2b
Adam Horky <adam.horky06@gmail.com>
2019-01-12 15:31:19 -0600

    trying to fix getting booked jobs for sitter

--- api/sitter-booked-jobs.php
+++ api/sitter-booked-jobs.php
@@ -1,54 +1,47 @@
 <?php
-  // error_reporting(E_ALL);
-  // ini_set("display_errors", 1);
-  
-  include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
 
-  if (!isset($_REQUEST) || !array_key_exists('user_id', $_REQUEST)) {
+include $_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php';
+
+if (!isset($_REQUEST) || !array_key_exists('user_id', $_REQUEST)) {
     echo json_encode(array('code' => 401, 'message' => 'User ID is required.'));
     exit;
-  }
-  
-  $sql = "SELECT
-      jm.job_id,
-      jm.set_code,
-      jm.family_user_id,
-      jm.booking_date,
-      jm.booking_placed_date,
-      jm.booking_date,
-      jm.no_of_kids,
-      jm.location_code,
-      jm.remarks,
-      jam.remarks AS your_remarks,
-      jm.sitter_approval,
-      jam.family_approval,
-      jam.sitter_user_id,
-      jam.applytime,
+}
+
+$user_id = $_REQUEST['user_id'];
+
+$sql = "SELECT
+      bm.book_id,
+      bm.family_user_id,
+      bm.sitter_user_id,
+      bm.booking_date,
+      bm.booking_placed_date,
+      bm.start_time,
+      bm.end_time,
+      bm.no_of_kids,
+      bm.location_code,
+      bm.remarks,
+      bm.sitter_approval,
       ui.user_first_name,
       ui.user_middle_name,
       ui.user_last_name
   FROM
-      job_management AS jm
-  JOIN jobapply_management AS jam ON jm.set_code = jam.job_id AND jam.sitter_user_id = ". $_REQUEST['user_id'] ."
-  LEFT JOIN user_management AS um ON um.user_id = jm.family_user_id
+      book_management AS bm
+  LEFT JOIN user_management AS um ON um.user_id = bm.family_user_id
   LEFT JOIN user_information AS ui ON ui.user_id = um.user_id
-  WHERE
-      jm.booking_status = 1
-  AND jm.sitter_approval = '0'
-  AND DATEDIFF(STR_TO_DATE(jm.booking_date, '%m/%d/%Y'), NOW()) > 0
-  GROUP BY
-      jm.set_code
+  WHERE bm.sitter_user_id = " . $user_id . "
+  AND DATEDIFF(STR_TO_DATE(bm.booking_date, '%m/%d/%Y'), NOW()) > 0
   ORDER BY
-      jm.job_id DESC;";
+      bm.book_id DESC;";
 
-  $query = mysql_query($sql);
-  $num = mysql_num_rows($query);
-  $data = array();
-  
-  while ($R = mysql_fetch_object($query)) {
-      $data[] = $R;
-  }
-  
-  $response = array('code' => 200, 'message' => array('results' => $data, 'total' => $num));
-  echo json_encode($response); exit;
-  
+$query = mysql_query($sql);
+$num = mysql_num_rows($query);
+$data = array();
+
+while ($R = mysql_fetch_object($query)) {
+    $data[] = $R;
+}
+
+$response = array('code' => 200, 'message' => array('results' => $data, 'total' => $num));
+echo json_encode($response);exit;
