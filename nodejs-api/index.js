99fbfae9b1e6893c45f7715c1244d360f550dc0e
Adam Horky <adam.horky06@gmail.com>
2019-03-31 15:16:53 -0400

    nodejs api

--- nodejs-api/index.js
+++ nodejs-api/index.js
@@ -1,9 +1,67 @@
 const { JWT } = require("google-auth-library");
 const https = require("https");
+const bodyParser = require("body-parser");
+
+const key = require("./service-account.json");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("YES"));
+app.post("/message", (req, res) => {
+  const { to, title, body, badge, sound, data } = req.body;
+  getAccessToken()
+    .then(token => {
+      const options = {
+        hostname: "fcm.googleapis.com",
+        path: "/v1/projects/our-sitter-list-30509/messages:send",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      };
+
+      const request = https.request(options, function(resp) {
+        resp.setEncoding("utf8");
+        resp.on("data", function(data) {
+          console.log("Message sent to Firebase for delivery, response:");
+          console.log(data);
+        });
+      });
+
+      request.on("error", function(err) {
+        console.log("Unable to send message to Firebase");
+        console.log(err);
+        res.status(500).json(err);
+      });
+
+      request.write(
+        JSON.stringify({
+          message: {
+            token: to,
+            notification: {
+              title,
+              body
+            },
+            data
+          }
+        })
+      );
+      request.end();
+
+      res.status(200).json({ message: "SUCCESS" });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+app.listen(port, () => console.log(`NodeJS listening on port ${port}!`));
 
 function getAccessToken() {
   return new Promise(function(resolve, reject) {
-    const key = require("./service-account.json");
     const jwtClient = new JWT(
       key.client_email,
       null,
@@ -19,49 +77,8 @@ function getAccessToken() {
         reject(err);
         return;
       }
+      console.log({ tokens });
       resolve(tokens.access_token);
     });
   });
 }
-
-getAccessToken().then(token => {
-  const options = {
-    hostname: "fcm.googleapis.com",
-    path: "/v1/projects/our-sitter-list-30509/messages:send",
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json"
-    }
-  };
-
-  const request = https.request(options, function(resp) {
-    resp.setEncoding("utf8");
-    resp.on("data", function(data) {
-      console.log("Message sent to Firebase for delivery, response:");
-      console.log(data);
-    });
-  });
-
-  request.on("error", function(err) {
-    console.log("Unable to send message to Firebase");
-    console.log(err);
-  });
-
-  const fcmMessage = {
-    message: {
-      token:
-        "de_6JQhf9TM:APA91bF9sWgjLIqq66k99RVFzQFGvczWmmEosgs1oWZN9ECXkLqplb_LVfovhwLGLkAUWZ6-eMOrqpU-zbcFfUYCh7cmtMroh1LRMsqW8LFLoIHFAK_Uxrt5xKP8nvZIlRifUHP02Yol",
-      notification: {
-        title: "Breaking News",
-        body: "New news story available."
-      },
-      data: {
-        thread_id: "sdfvmksdfknjl"
-      }
-    }
-  };
-
-  request.write(JSON.stringify(fcmMessage));
-  request.end();
-});
