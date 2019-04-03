f988d4199f0eb31172f41ad780a0a54c54d02499
Adam Horky <adam.horky06@gmail.com>
2019-04-03 18:56:50 -0500

    fixing

--- api/update-blocked-user.php
+++ api/update-blocked-user.php
@@ -15,7 +15,7 @@ $blocked_user_id = $_REQUEST['blocked_user_id'];
 $block_state = $_REQUEST['block_state'];
 
 if ($block_state === 'unblock') {
-    $sql_DeleteExisting = "DELETE FROM blocked_users
+    $sql_DeleteExisting = "DELETE FROM blocked_users b
                            WHERE b.user_id='" . $user_id . "' AND b.blocked_user_id='" . $blocked_user_id . "'";
     mysql_query($sql_DeleteExisting);
     $response = array('code' => 200, 'message' => 'Unblocked');
