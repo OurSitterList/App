9927618b5b3e04088d672ab6a01f390fa0e9a483
Adam Horky <adam.horky06@gmail.com>
2019-04-28 11:18:38 -0500

    update to query

--- api/get-message-thread.php
+++ api/get-message-thread.php
@@ -5,18 +5,28 @@
 
 include $_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php';
 
+$user_id = $_REQUEST['user_id'];
+$recipient_id = $_REQUEST['recipient_id'];
+
 $query = mysql_query("SELECT DISTINCT thread_id
                       FROM messages
-                      WHERE user_id = 139 AND recipient_id = 363");
+                      WHERE user_id = '" . $user_id . "' AND recipient_id = '" . $recipient_id . "'
+                      OR user_id = '" . $recipient_id . "' AND recipient_id = '" . $user_id . "' LIMIT 1");
 
 $json = array();
 if (mysql_num_rows($query) > 0) {
     $R = mysql_fetch_object($query);
     $json['thread_id'] = $R->thread_id;
-    $response = array('code' => 200, 'result' => $json);
+
+    header('Content-Type:application/json');
+    http_response_code(200);
+    $response = array('result' => $json);
     echo json_encode($response);exit;
 } else {
     $json['thread_id'] = null;
-    $response = array('code' => 200, 'result' => $json);
+    $response = array('result' => $json);
+
+    header('Content-Type:application/json');
+    http_response_code(200);
     echo json_encode($response);exit;
 }
