d8424b85117590bfd8d5be45e65c7433ae19e2f8
Adam Horky <adam.horky06@gmail.com>
2019-06-19 18:10:30 -0500

    registration

--- api/sitter-register.php
+++ api/sitter-register.php
@@ -47,7 +47,7 @@
 // error_reporting(E_ALL);
 // ini_set("display_errors", 1);
 
-include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
+include $_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php';
 
 // var_dump($_POST);
 // exit;
@@ -55,94 +55,97 @@ include($_SERVER["DOCUMENT_ROOT"] . '/includes/connection.php');
 extract($_POST);
 
 try {
-  $firstName 				= trim($user_first_name);
-  $lastName 				= trim($user_last_name);
-  $creditCardNumber 		= trim($cardNumber);
-  $expDateMonth 			= trim($expirationDate_month);
-  $padDateMonth 			= str_pad(trim($expDateMonth), 2, '0', STR_PAD_LEFT);
-  $expDateYear 			= trim($expirationDate_year);
-  $cvv2Number 			= trim($cardCode);
-  $amount 				= "25.00";
-  $currencyCode			= "USD";
-  $paymentType			= "AUTH_CAPTURE";
-  $date 					= $expDateMonth.$expDateYear;
+    $firstName = trim($user_first_name);
+    $lastName = trim($user_last_name);
+    $creditCardNumber = trim($cardNumber);
+    $expDateMonth = trim($expirationDate_month);
+    $padDateMonth = str_pad(trim($expDateMonth), 2, '0', STR_PAD_LEFT);
+    $expDateYear = trim($expirationDate_year);
+    $cvv2Number = trim($cardCode);
+    $amount = "25.00";
+    $currencyCode = "USD";
+    $paymentType = "AUTH_CAPTURE";
+    $date = $expDateMonth . $expDateYear;
 
-  $user_details			= array (
-    "user_name"				=> $sitter_username,
-    "user_password"			=> $sitter_password,
-    "user_email"			=> $sitter_email,
-    "join_date"				=> time(),
-    "user_type"				=> 'sitter',
-    "location_code"			=> $user_zip,
-    "user_first_name"		=> $user_first_name,
-    "user_last_name"		=> $user_last_name,
-    "user_driver_license"	=> $user_driver_license,
-    "user_firstaid_training"			=> $user_firstaid_training,
-    "user_date_of_certification"		=> $user_date_of_certification,
-    "is_user_willing_to_certified"		=> $is_user_willing_to_certified,
-    "user_cpr_training"					=> $user_cpr_training,
-    "user_date_of_certification_cpr"	=> $user_date_of_certification_cpr,
-    "is_user_willing_to_certified_cpr"	=> $is_user_willing_to_certified_cpr,
-    "user_emergency_contact"			=> $user_emergency_contact,
-    "user_description"		=> $user_description,
-    "user_cell_phone"		=> $user_cell_phone,
-    "user_high_school"		=> $user_high_school,
-    "user_high_school_name"	=> $user_high_school_name,
-    "user_college"			=> $user_college,
-    "user_college_name"		=> $user_college_name,
-    "user_ref1_name"		=> $user_ref1_name,
-    "user_ref1_role"		=> $user_ref1_role,
-    "user_ref1_age"			=> $user_ref1_age,
-    "user_ref1_length"		=> $user_ref1_length,
-    "user_ref2_name"		=> $user_ref2_name,
-    "user_ref2_role"		=> $user_ref2_role,
-    "user_ref2_age"			=> $user_ref2_age,
-    "user_ref2_length"		=> $user_ref2_length,
-    "user_biography"		=> $user_first_name,
-    "user_newborn_exp"		=> $user_babysitting_exp
-  );
+    $user_details = array(
+        "user_name" => $sitter_username,
+        "user_password" => $sitter_password,
+        "user_email" => $sitter_email,
+        "join_date" => time(),
+        "user_type" => 'sitter',
+        "location_code" => $user_zip,
+        "user_first_name" => $user_first_name,
+        "user_last_name" => $user_last_name,
+        "user_driver_license" => $user_driver_license,
+        "user_firstaid_training" => $user_firstaid_training,
+        "user_date_of_certification" => $user_date_of_certification,
+        "is_user_willing_to_certified" => $is_user_willing_to_certified,
+        "user_cpr_training" => $user_cpr_training,
+        "user_date_of_certification_cpr" => $user_date_of_certification_cpr,
+        "is_user_willing_to_certified_cpr" => $is_user_willing_to_certified_cpr,
+        "user_emergency_contact" => $user_emergency_contact,
+        "user_description" => $user_description,
+        "user_cell_phone" => $user_cell_phone,
+        "user_high_school" => $user_high_school,
+        "user_high_school_name" => $user_high_school_name,
+        "user_college" => $user_college,
+        "user_college_name" => $user_college_name,
+        "user_ref1_name" => $user_ref1_name,
+        "user_ref1_role" => $user_ref1_role,
+        "user_ref1_age" => $user_ref1_age,
+        "user_ref1_length" => $user_ref1_length,
+        "user_ref2_name" => $user_ref2_name,
+        "user_ref2_role" => $user_ref2_role,
+        "user_ref2_age" => $user_ref2_age,
+        "user_ref2_length" => $user_ref2_length,
+        "user_biography" => $user_first_name,
+        "user_newborn_exp" => $user_babysitting_exp,
+    );
 
-  $setting 		= new Setting(array('id' => 8));
-  $setting_item	= $setting::get_setting_item();
-  $login		= $setting_item[3];
+    $setting = new Setting(array('id' => 8));
+    $setting_item = $setting::get_setting_item();
+    $login = $setting_item[3];
 
-  $setting 		= new Setting(array('id' => 9));
-  $setting_item	= $setting::get_setting_item();
-  $transkey		= $setting_item[3];
+    $setting = new Setting(array('id' => 9));
+    $setting_item = $setting::get_setting_item();
+    $transkey = $setting_item[3];
 
-  $payment_details = array(
-    "x_login"			=> "$login",
-    "x_tran_key"		=> "$transkey",
-    "x_version"			=> "3.1",
-    "x_delim_data"		=> "TRUE",
-    "x_delim_char"		=> "|",
-    "x_relay_response"	=> "FALSE",
-    "x_device_type"		=> "1",
-    "x_type"			=> "AUTH_CAPTURE",
-    "x_method"			=> "CC",
-    "x_card_num"		=> $creditCardNumber,
-    "x_exp_date"		=> $date,
-    "x_amount"			=> $amount,
-    "x_description"		=> "{$firstName} {$lastName} one time registration fee.",
-    "x_first_name"		=> $firstName,
-    "x_last_name"		=> $lastName,
-    "x_response_format"	=> "1",
-    "is_payment_test"	=> 1
-  );
+    $payment_details = array(
+        "x_login" => "$login",
+        "x_tran_key" => "$transkey",
+        "x_version" => "3.1",
+        "x_delim_data" => "TRUE",
+        "x_delim_char" => "|",
+        "x_relay_response" => "FALSE",
+        "x_device_type" => "1",
+        "x_type" => "AUTH_CAPTURE",
+        "x_method" => "CC",
+        "x_card_num" => $creditCardNumber,
+        "x_exp_date" => $date,
+        "x_amount" => $amount,
+        "x_description" => "{$firstName} {$lastName} one time registration fee.",
+        "x_first_name" => $firstName,
+        "x_last_name" => $lastName,
+        "x_response_format" => "1",
+        "is_payment_test" => 1,
+    );
 
-  $auth	= new Auth();
-  $response = $auth->register($user_details, $payment_details, true);
+    header('Content-Type: application/json');
+    $response = array('code' => 200, 'message' => $user_details);
+    echo json_encode($response);exit;
 
-  if ($response->success && isset($response->reason)) {
-    // $response	= array('code' => 200, 'message' => $response->reason.$notify);
-    $response	= array('code' => 200, 'message' => $user_details);
-  } else {
-    $response	= array('code' => 400, 'message' => $response->reason);
-  }
+    // $auth    = new Auth();
+    // $response = $auth->register($user_details, $payment_details, true);
 
-  echo json_encode($response); exit;
+    // if ($response->success && isset($response->reason)) {
+    //   $response    = array('code' => 200, 'message' => $response->reason.$notify);
+    // } else {
+    //   $response    = array('code' => 400, 'message' => $response->reason);
+    // }
+
+    // echo json_encode($response); exit;
 
 } catch (Exception $e) {
-  $response	= array('code' => 400, 'message' => $e->getMessage());
-  echo json_encode($response); exit;
+    $response = array('code' => 400, 'message' => $e->getMessage());
+    echo json_encode($response);exit;
 }
