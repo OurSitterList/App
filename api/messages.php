85bd16c98662168752172d181076ccbfe677586e
Adam Horky <adam.horky06@gmail.com>
2019-08-02 09:51:54 -0500

    messaging

--- api/messages.php
+++ api/messages.php
@@ -11,12 +11,16 @@ if (!isset($_REQUEST) || !array_key_exists('user_id', $_REQUEST)) {
 }
 
 $sql1 = "SELECT m.id, m.user_id as user_id, m.recipient_id as recipient_id, m.thread_id, m.message, ui.user_first_name as user_first_name, ui.user_last_name as user_last_name, uir.user_first_name as recipient_first_name, uir.user_last_name as recipient_last_name
-         FROM messages m LEFT JOIN user_information ui ON m.user_id = ui.user_id LEFT JOIN user_information uir ON m.recipient_id = uir.user_id
-         WHERE m.user_id='" . $_REQUEST['user_id'] . "' OR m.recipient_id = '" . $_REQUEST['user_id'] . "' AND m.deleted_at IS NULL;";
+         FROM messages m
+         LEFT JOIN user_information ui ON m.user_id = ui.user_id
+         LEFT JOIN user_information uir ON m.recipient_id = uir.user_id
+         WHERE m.user_id='" . $_REQUEST['user_id'] . "' AND m.recipient_id = '" . $_REQUEST['recipient_id'] . "' AND m.deleted_at IS NULL;";
 
 $sql2 = "SELECT m.id, m.user_id as user_id, m.recipient_id as recipient_id, m.thread_id, m.message, ui.user_first_name as user_first_name, ui.user_last_name as user_last_name, uir.user_first_name as recipient_first_name, uir.user_last_name as recipient_last_name
-         FROM messages m LEFT JOIN user_information ui ON m.user_id = ui.user_id LEFT JOIN user_information uir ON m.recipient_id = uir.user_id
-         WHERE m.user_id='" . $_REQUEST['recipient_id'] . "' OR m.recipient_id = '" . $_REQUEST['recipient_id'] . "' AND m.deleted_at IS NULL;";
+         FROM messages m
+         LEFT JOIN user_information ui ON m.user_id = ui.user_id
+         LEFT JOIN user_information uir ON m.recipient_id = uir.user_id
+         WHERE m.user_id='" . $_REQUEST['recipient_id'] . "' AND m.recipient_id = '" . $_REQUEST['user_id'] . "' AND m.deleted_at IS NULL;";
 
 $query1 = mysql_query($sql1);
 $query2 = mysql_query($sql2);
