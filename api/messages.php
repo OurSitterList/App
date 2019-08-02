13493eda0c3344666c1fb7fc295b3cc7f89c8740
Adam Horky <adam.horky06@gmail.com>
2019-08-02 10:04:49 -0500

    messaging

--- api/messages.php
+++ api/messages.php
@@ -27,9 +27,11 @@ $query2 = mysql_query($sql2);
 $data = array();
 
 while ($R = mysql_fetch_object($query1)) {
+    $R['id'] = (int) $R['id'];
     $data[] = $R;
 }
 while ($R = mysql_fetch_object($query2)) {
+    $R['id'] = (int) $R['id'];
     $data[] = $R;
 }
 
