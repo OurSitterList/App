6f05d74908b1df5c6be0986ef4e13a0f28de94a2
hiten <hiten03_shah@yahoo.com>
2020-11-21 22:13:25 +0530

    alert added.

--- js/core.js
+++ js/core.js
@@ -69,9 +69,13 @@ function getLoginBtnHandler(type)
                 }
                 else if (data && typeof data.red != 'undefined' && data.red)
                 {
-                    console.log(data);
-                    alert(data.red);
-                    document.location = data.red;
+                    if(type == 'family'){
+                        document.location = 'family_dashboard.php';
+                    }
+                    else{
+                     document.location = 'sitter_dashboard.php';  
+                    }
+                    // document.location = data.red;
                 }
                 else
                 {
